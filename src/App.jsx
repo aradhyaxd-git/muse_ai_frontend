2794fcc0b4720f7ef89bb1a1ac0f44a621ad88de
Aradhya <2k23.cs2312107@gmail.com>
2025-11-05 15:05:41 +0530

    fix: fixed app.jsx layout , rendering sidebar and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-// import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import WriteArticle from "./pages/WriteArticle";
 import BlogTitles from "./pages/BlogTitles";
 import GenerateImages from "./pages/GenerateImages";
 import RemoveBackground from "./pages/RemoveBackground";
 import RemoveObject from "./pages/RemoveObject";
 import ReviewResume from "./pages/ReviewResume";
-// import Community from "./pages/Community";
+import Community from "./pages/Community";
+import Layout from "./pages/Layout";
 // import AuthModal from "./components/features/AuthModal";
 
 function App() {
@@ -16,14 +17,16 @@ function App() {
   <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/ai/write-article" element={<WriteArticle />} />
-      <Route path="/ai/blog-titles" element={<BlogTitles />} />
-      <Route path="/ai/generate-images" element={<GenerateImages />} />
-      <Route path="/ai/remove-background" element={<RemoveBackground />} />
-      <Route path="/ai/remove-object" element={<RemoveObject />} />
-      <Route path="/ai/review-resume" element={<ReviewResume />} />
-      {/* <Route path="/ai" element={<Dashboard />} />
-      <Route path="/ai/community" element={<Community />} /> */}
+      <Route path="/ai" element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="write-article" element={<WriteArticle />} />
+        <Route path="blog-titles" element={<BlogTitles />} />
+        <Route path="generate-images" element={<GenerateImages />} />
+        <Route path="remove-background" element={<RemoveBackground />} />
+        <Route path="remove-object" element={<RemoveObject />} />
+        <Route path="review-resume" element={<ReviewResume />} />
+        <Route path="community" element={<Community />} />
+      </Route>
     </Routes>
   </>
   );
